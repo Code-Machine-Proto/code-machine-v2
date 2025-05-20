7656a2e2038ec608241f40144e44dd5f7dae1fc2
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-05-20 12:51:49 -0400

    fix(ui): Uniformiser les liens des processeurs

--- frontend/src/components/BoxLink.tsx
+++ frontend/src/components/BoxLink.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 
 export default function BoxLink ({ nom, url }: BoxLinkProps) {
     return (
-        <Link to={ url } className="text-white bg-main p-[2rem] rounded-xl size-fit">
-            { nom }
+        <Link to={ url } className="flex bg-main size-[7rem] justify-center rounded-xl p-3">
+            <p className="text-white self-center">{nom}</p>
         </Link>
     );
 }
