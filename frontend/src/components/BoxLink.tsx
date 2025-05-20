a5619e5d4d18d28b7fae4162f03e3432806f946a
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-05-20 13:01:40 -0400

    feat(ux): Ajout de l'effet de hover sur les liens

--- frontend/src/components/BoxLink.tsx
+++ frontend/src/components/BoxLink.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export default function BoxLink ({ nom, url }: BoxLinkProps) {
     return (
-        <Link to={ url } className="flex bg-main size-[7rem] justify-center rounded-xl p-3">
+        <Link to={ url } className="flex bg-main-950 size-[7rem] justify-center rounded-xl p-3 hover:bg-main-800">
             <p className="text-white self-center">{nom}</p>
         </Link>
     );
