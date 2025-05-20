4802e8ca2569e36f6394390cfe0f66dacef5cc59
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-05-20 13:52:44 -0400

    fix(ux): Ajouter le lien pour le titre du site

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import logo from "@src/assets/code-machine-logo.png";
+import { Link } from "react-router";
 
 export default function Header() {
     return (
-    <div className="flex h-[5rem] w-full bg-main-950 justify-center items-center gap-5" >
+    <Link to="/" className="flex h-[5rem] w-full bg-main-950 justify-center items-center gap-5" >
         <img src={ logo } alt="Logo" className="h-[3rem]" />
         <p className="text-white text-xl">Code Machine</p>
-    </div>
+    </Link>
     );
 }
