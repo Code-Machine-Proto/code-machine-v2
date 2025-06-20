3b6b2f182762ab21d72891da34890b94f8e0b325
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-06-20 15:55:11 -0400

    doc(parts): Ajout de la doc pour quelque partie des processeurs

--- frontend/src/components/processor/parts/Bus.tsx
+++ frontend/src/components/processor/parts/Bus.tsx
@@ -1,5 +1,13 @@
 import type { BusProps } from "@src/interface/props/ProcessorParts";
 
+/**
+ * Affiche une ligne et un nombre pour représenter la taille du bus. 
+ * Il faut le placer au dessus d'un fil dans un processeur
+ * @prop x - Position en x du svg
+ * @prop y - Position en y du svg
+ * @prop number - Taille du bus
+ * @returns le svg représentant la taille du bus
+ */
 export default function Bus({ x, y, number }: BusProps) {
     return (
         <svg x={x} y={y} width="19" height="19" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
