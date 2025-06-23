049072ca2565857d5df0e542345c27d3ae0cc2eb
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-06-23 15:15:27 -0400

    doc(frontend): Ajout de la doc pour quelque composant

--- frontend/src/class/MaAccumulator.ts
+++ frontend/src/class/MaAccumulator.ts
@@ -1,6 +1,9 @@
 import { ProcessorId } from "@src/interface/CodeInterface";
 import Processor from "./Processor";
 
+/**
+ * Classe représentant l'état du processeur à accumulateur avec Ma
+ */
 export default class MaAccumulator extends Processor {
     defaultCode = "";
 
--- frontend/src/class/PolyRisc.ts
+++ frontend/src/class/PolyRisc.ts
@@ -1,6 +1,9 @@
 import { ProcessorId } from "@src/interface/CodeInterface";
 import Processor from "./Processor";
 
+/**
+ * Classe représentant l'état du processeur PolyRisc
+ */
 export default class PolyRisc extends Processor {
     defaultCode = "";
 
