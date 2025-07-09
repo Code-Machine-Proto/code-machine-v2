e8edd121c036bf0f90b1b75370e0e6c6c58edea1
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-07-09 08:33:54 -0400

    feat(couleur): Ajout de la couleur pour le syntax highlighting

--- frontend/src/class/visitor/HighligthSyntax.ts
+++ frontend/src/class/visitor/HighligthSyntax.ts
@@ -2,12 +2,15 @@ import type { Visitor } from "@src/interface/VisitorInterface";
 import type Accumulator from "../Accumulator";
 import type MaAccumulator from "../MaAccumulator";
 import type PolyRisc from "../PolyRisc";
+import type { HighlightedLine } from "@src/interface/HighlightedLines";
 
 export class HighlightSyntaxVisitor implements Visitor {
     visitAccumulator(processor: Accumulator): void{
+        const LABEL_REGEX = "[a-zA-Z][a-zA-Z0-9_-]+:";
         const lines = processor.lines;
         processor.highlightedText = lines.map((line) => {
-            return [{ text: line, color: "text-white"}];
+            const highlightedLine: HighlightedLine = [];
+            return highlightedLine;
         });
     }
 
