78c030cccbca2db7199b944b29be6abcf55397ec
FelixAntoineLefebvre <felix-antoine-2.lefebvre@polymtl.ca>
2025-07-11 11:09:39 -0400

    fix(lint): Régler les erreurs de lint

--- frontend/src/constants/SyntaxColor.ts
+++ frontend/src/constants/SyntaxColor.ts
@@ -1,13 +1,12 @@
-import type { TokenType } from "@src/interface/visitor/Token";
-import { TokenType as Token } from "@src/interface/visitor/Token";
+import { TokenType } from "@src/interface/visitor/Token";
 
 export const SYNTAX_COLOR: Record<TokenType, string> = {
-    [Token.NUMBER]: "text-white",
-    [Token.BLANK]: "text-white",
-    [Token.COMMENT]: "text-comment",
-    [Token.LABEL]: "text-label",
-    [Token.MAIN_LABEL]: "text-white",
-    [Token.OPERATION]: "text-inst",
-    [Token.WORD]: "text-white",
-    [Token.REGISTER]: "text-register",
+    [TokenType.NUMBER]: "text-white",
+    [TokenType.BLANK]: "text-white",
+    [TokenType.COMMENT]: "text-comment",
+    [TokenType.LABEL]: "text-label",
+    [TokenType.MAIN_LABEL]: "text-white",
+    [TokenType.OPERATION]: "text-inst",
+    [TokenType.WORD]: "text-white",
+    [TokenType.REGISTER]: "text-register",
 };
