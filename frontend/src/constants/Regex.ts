6a3bc6007b982c58dec410cbe0bae4a3a2a72214
FelixAntoineLefebvre <144398518+FelixAntoineLefebvre@users.noreply.github.com>
2026-01-04 23:52:27 -0500

    fix: Régler le problème de reconstruction de code (#110)

--- frontend/src/constants/Regex.ts
+++ frontend/src/constants/Regex.ts
@@ -66,6 +66,6 @@ export const IMM_LOAD_REGEX = /^ldi$/;
  */
 export const REGISTER_POLYRISC = /^\(?r([0-9]|[1-2][0-9]|3[1-2])\)?,?$/;
 
-export const SIMPLE_REGISTER_POLYRISC = /r([0-9]|[1-2][0-9]|3[1-2])/;
+export const SIMPLE_REGISTER_POLYRISC = /r(3[1-2]|[1-2][0-9]|[0-9])/;
 
 export const IDENTIFICATION_PARANTHESE = /\(.*\)/;
