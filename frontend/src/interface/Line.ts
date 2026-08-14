3610525af2d25fe0d9f427a4a27814a8ef0e89ce
Abdul Chaarani <75145721+abdulchaarani@users.noreply.github.com>
2026-08-14 09:53:12 -0400

    Revert "Mise à jour 26.3.0 (#152)" (#154)
    This reverts commit 91278627821bc6ae8be5144313a4d0c8a43b81ca.

--- frontend/src/interface/Line.ts
+++ frontend/src/interface/Line.ts
@@ -2,50 +2,49 @@
  * Les états des lignes de l'accumulateur
  */
 export enum LineStateAccumulator {
-  error = -1,
-  fetch,
-  load,
-  store,
-  decode,
-  alu,
-  nop,
-  branching,
+    error = -1,
+    fetch,
+    load,
+    store,
+    decode,
+    alu,
+    nop,
+    branching,
 }
 
 /**
  * Les états des lignes de l'accumulateur avec MA
  */
 export enum LineStateMa {
-  error = -1,
-  fetch,
-  decode,
-  addSubMul,
-  addSubA,
-  addSubX,
-  sh,
-  store,
-  load,
-  loadA,
-  loadI,
-  storeA,
-  storeI,
-  lea,
-  branching,
-  nop,
+    error = -1,
+    fetch,
+    decode,
+    addSubMul,
+    addSubA,
+    addSubX,
+    sh,
+    store,
+    load,
+    loadA,
+    loadI,
+    storeA,
+    storeI,
+    branching,
+    nop,
 }
 
 /**
  * Les états des lignes du PolyRisc
  */
 export enum LineStatePolyRisc {
-  error = -1,
-  fetch,
-  decode,
-  opTwoReg,
-  opThreeReg,
-  load,
-  store,
-  loadI,
-  branching,
-  nop,
+    error = -1,
+    fetch,
+    decode,
+    opTwoReg,
+    opThreeReg,
+    load,
+    store,
+    loadI,
+    branching,
+    nop,
 }
