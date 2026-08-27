4eb4fd75fdf3cff1b68d17b4fd863b3d579b0e20
abdulchaarani <abdul@chaarani.ca>
2026-08-27 09:45:53 -0400

    Feat(frontend): save & load code files

--- frontend/src/constants/ConfirmationModal.ts
+++ frontend/src/constants/ConfirmationModal.ts
@@ -10,3 +10,5 @@ export const DEFAULT_CONFIRMATION_MODAL: ConfirmationModalInterface = {
 };
 
 export const DELETE_CODE_MESSAGE = "Cette action va enlever le code de l'éditeur de texte et est irréversible. Êtes vous bien sur de vouloir procéder";
+
+export const UPLOAD_CODE_MESSAGE = "Cette action va remplacer le code actuel de l'éditeur de texte par le contenu du fichier importé et est irréversible. Êtes vous bien sur de vouloir procéder";
